a076b8e711e2a22e3baa1a0b51b3d886cedad348
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-06-05 17:17:34 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,9 +3,7 @@ node {
    // ws('C:\\Apps\\Jenkins\\jobs\\trial') {
     // requires SonarQube Scanner 2.8+
     def scannerHome = tool 'Sonar Runner';
-    withSonarQubeEnv('Sonar') {
-	   bat "gradlew -Dsonar.analysis.mode "
-     // bat "${scannerHome}/StartSonar.bat"
+    sh "${scannerHome}/bin/sonar-scanner"
     }
   
 }
